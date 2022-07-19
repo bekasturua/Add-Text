a43b4041fe269896bec437a09be9ca5dfe939665
Beka Sturua <beqasturua3@gmail.com>
2022-07-19 12:37:44 +0400

    send request with POST method

--- src/App.js
+++ src/App.js
@@ -41,8 +41,19 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-e8a4a-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
